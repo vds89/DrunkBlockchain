739d1449baa9354d288ae8b68f1096807d0852ec
Vincenzo De Simone <vincenzodesimone04@gmail.com>
2018-10-13 21:04:11 +0200

    Finish Wallet-Transaction-Block

--- src/TransactionInput.java
+++ src/TransactionInput.java
@@ -9,5 +9,11 @@
  * @author pcvincenzo
  */
 public class TransactionInput {
-    
+    public String transactionOutputId; //Reference to TransactionOutputs -> transactionId
+    public TransactionOutput UTXO; //Contains the Unspent transaction output
+	
+    //Constructor
+    public TransactionInput(String transactionOutputId) {
+	this.transactionOutputId = transactionOutputId;
+    }
 }
